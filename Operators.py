e16fcdfe167aef31691bc24e7391d705b308ac30
Chanchal Narang <smile.narang1990@gmail.com>
2024-09-30 16:12:05 +0530

    Operators.py
    Operator + behaviour with different 
    Datatypes

---
+++ Operators.py
@@ -0,0 +1,17 @@
+
+i = 4
+d = 4.0
+s = 'HackerRank'
+# Declare second integer, double, and String variables.
+second_Integer=int(input("Enter secondInteger: "))
+second_double=float(input("Enter second Double value: "))
+second_string=input("Enter second string:")
+# Read and save an integer, double, and String to your variables.
+integer_sum= i+second_Integer
+print(integer_sum)
+# Print the sum of both integer variables on a new line.
+double_sum=d+second_double
+print(double_sum)
+# Print the sum of the double variables on a new line.
+string_concatenate=s+second_string
+print(string_concatenate)
